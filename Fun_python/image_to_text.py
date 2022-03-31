0898cf56344463b4b30678d97a4c6fc17918609e
Jafar Sherazi <101875212+Jafar089@users.noreply.github.com>
2022-04-01 04:09:00 +0500

    Add files via upload

---
+++ Fun_python/image_to_text.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.image_to_ascii_art('jafar.jpeg','png_to_art')
---
+++ Fun_python/whatsapp1.py
@@ -0,0 +1,18 @@
+# import pyautogui as spam
+# import time
+
+# limit = int(input('Enter your limit: '))
+# msg = input('Enter your message: ')
+# i=0
+
+# time.sleep(10)
+
+# while i< limit:
+
+#     spam.typewrite(msg)
+#     spam.press('Enter')
+
+#     i+=1
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+923215044572', 'Hello Jafar', 9, 13)
